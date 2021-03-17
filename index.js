8a010ef88c1c5d6697494ab7816f652635328e10
Yashar Zolmajdi <yashar.zolmajdi@gmail.com>
2021-03-17 08:15:51 +0700

    Add playground for production build

--- index.js
+++ index.js
@@ -31,7 +31,12 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
 
 server.listen(process.env.PORT || 4000).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
